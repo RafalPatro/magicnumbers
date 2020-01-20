f1dd14dd0aed95211793fa1a48a4696127a75b44
rafalp <rafal.patro@gmail.com>
2020-01-20 21:19:50 +0100

    Added exceptions handling

--- src/pl/patro/MagicNumbersService.java
+++ src/pl/patro/MagicNumbersService.java
@@ -2,6 +2,7 @@ package pl.patro;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
@@ -9,13 +10,20 @@ import java.util.stream.Collectors;
 
 public class MagicNumbersService {
 
-    byte[] getFilesMagicNumbers(String fileName, int size) throws IOException {
+    byte[] getFilesMagicNumbers(String fileName, int size) {
         File file = new File(fileName);
         try(FileInputStream fileInputStream = new FileInputStream(file)) {
             byte[] fileBytes = new byte[size];
             fileInputStream.read(fileBytes);
             return fileBytes;
+        } catch (FileNotFoundException e) {
+           System.out.println("File no exist");
+           System.exit(2);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(3);
         }
+        return new byte[0];
     }
 
     String decodeExtension(Map<byte[],String> map, byte[] fileBytes){
